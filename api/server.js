c04107b7902fe3bab34ac0027e61b3948642796a
trannam <namphongtran232@gmail.com>
2023-08-15 00:02:20 +0700

    Use the Project as a Module, update vercel config

--- api/server.js
+++ api/server.js
@@ -1,15 +1,31 @@
 // See https://github.com/typicode/json-server#module
 const jsonServer = require('json-server')
+const clone = require('clone')
+const data = require('../db.json')
+
+
+const isProductionEnv = process.env.NODE_ENV === 'production';
 const server = jsonServer.create()
-const router = jsonServer.router('db.json')
+
+const router = jsonServer.router(isProductionEnv ? clone(data) : 'db.json', {
+    _isFake: isProductionEnv
+})
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
+
+server.use((req, res, next) => {
+    if (req.path !== '/')
+        router.db.setState(clone(data))
+    next()
+})
+
 // Add this before server.use(router)
 server.use(jsonServer.rewriter({
     '/api/*': '/$1',
     '/blog/:resource/:id/show': '/:resource/:id'
 }))
+
 server.use(router)
 server.listen(3000, () => {
     console.log('JSON Server is running')
